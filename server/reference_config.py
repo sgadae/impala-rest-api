ffad76f9227002b9c1deb6948e8321c1742bda57
sgadae <44564422+sgadae@users.noreply.github.com>
2019-04-01 23:36:51 +0530

    Update reference_config.py

--- server/reference_config.py
+++ server/reference_config.py
@@ -1,15 +1,15 @@
 # Server
 HOST = '0.0.0.0'
-PORT = 5000
+PORT = 21050
 DEBUG_MODE = True
 
 # Impala connection
-IMPALA_HOST = 'localhost'
+IMPALA_HOST = 'npuscoldhen01.int.vertivco.com'
 IMPALA_PORT = 21050
 
 # Redis connection
 REDIS_URL = 'redis://redis:6379'
 
 # API settings
-SECURITY_TOKEN = '<put-security-token-here>'
+SECURITY_TOKEN = 'Q2hhcnVkZGF0dGEuWWVsbmU6Q0BtZUxldHNHbyMx'
 MAX_RECORDS_IN_RESPONSE = 1000
